5868773664fb51935c738e8050dd2707a1cc1650
Michael Abbott <michael@mjabbottdesign.com>
2021-11-09 18:26:42 -0800

    Create migrations for customers, addresses, teas and subscriptions

---
+++ db/migrate/20211110012340_create_subscriptions.rb
@@ -0,0 +1,15 @@
+class CreateSubscriptions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subscriptions do |t|
+      t.integer :price
+      t.integer :status
+      t.integer :frequency
+      t.integer :ounces
+      t.references :customer, foreign_key: true
+      t.references :tea, foreign_key: true
+      t.references :address, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
